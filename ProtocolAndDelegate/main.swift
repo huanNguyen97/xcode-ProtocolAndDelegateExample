ca58562fa927038d7be4cbc75b6eae3ea8313d42
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-11 14:09:18 +0700

    Newbie with Swift...

--- ProtocolAndDelegate/main.swift
+++ ProtocolAndDelegate/main.swift
@@ -1,11 +1,59 @@
-//
-//  main.swift
-//  ProtocolAndDelegate
-//
-//  Created by BSP Macbook on 3/11/22.
-//
 
-import Foundation
+protocol AdvanceLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvanceLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happen")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
 
+struct Paramedic: AdvanceLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Checking 30 seconds")
+    }
+}
+
+class Doctor: AdvanceLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Doctor is coming")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart breaking sound")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whriirrr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
